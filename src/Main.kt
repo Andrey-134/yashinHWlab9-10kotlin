2fd044292c2009d7b8749d6c81e7a81d1b1177f3
Andrey-134 <aasin2753@gmail.com>
2026-01-22 19:52:56 +0300

    Implement DevelopmentDepartment and TestingDepartment

--- src/Main.kt
+++ src/Main.kt
@@ -15,41 +15,51 @@
 //    println("Опыт: ${employee.getYearsOfExperience()} лет")
 //}
 
+//fun main() {
+//    val task1 = Task(
+//        title = "Задача 1",
+//        description = "Описание",
+//        priority = Priority.HIGH,
+//        isCompleted = false
+//    )
+//    val task2 = Task(
+//        "Задача 1",
+//        "Описание",
+//        Priority.HIGH,
+//        false
+//    )
+//
+//    val task3 = Task(
+//        "Задача 2",
+//        "Другое описание",
+//        Priority.LOW,
+//        true
+//    )
+//
+//    println("Свойства task1:")
+//    println("  title: ${task1.title}")
+//    println("  description: ${task1.description}")
+//    println("  priority: ${task1.priority}")
+//    println("  isCompleted: ${task1.isCompleted}")
+//    println("\ntask1 == task2: ${task1 == task2}")
+//    println("task1 == task3: ${task1 == task3}")
+//    println("\nХеши: ${task1.hashCode()}, ${task2.hashCode()}, ${task3.hashCode()}")
+//
+//    println("\ntask1: $task1")
+//
+//    val task4 = task1.copy(
+//        priority = Priority.MEDIUM,
+//        isCompleted = true
+//    )
+//    println("\nКопия task4: $task4")
+//}
+
 fun main() {
-    val task1 = Task(
-        title = "Задача 1",
-        description = "Описание",
-        priority = Priority.HIGH,
-        isCompleted = false
-    )
-    val task2 = Task(
-        "Задача 1",
-        "Описание",
-        Priority.HIGH,
-        false
-    )
+    println("\n Демонстрация наследования ")
 
-    val task3 = Task(
-        "Задача 2",
-        "Другое описание",
-        Priority.LOW,
-        true
-    )
+    val devDept = DevelopmentDepartment()
+    val testDept = TestingDepartment()
 
-    println("Свойства task1:")
-    println("  title: ${task1.title}")
-    println("  description: ${task1.description}")
-    println("  priority: ${task1.priority}")
-    println("  isCompleted: ${task1.isCompleted}")
-    println("\ntask1 == task2: ${task1 == task2}")
-    println("task1 == task3: ${task1 == task3}")
-    println("\nХеши: ${task1.hashCode()}, ${task2.hashCode()}, ${task3.hashCode()}")
-
-    println("\ntask1: $task1")
-
-    val task4 = task1.copy(
-        priority = Priority.MEDIUM,
-        isCompleted = true
-    )
-    println("\nКопия task4: $task4")
+    devDept.printDepartmentGoal()
+    testDept.printDepartmentGoal()
 }
